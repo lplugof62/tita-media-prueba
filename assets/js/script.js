e28a3cab5f6d15ec4ee0df725f91f78b46f1da71
lplugof62 <lplugof@gmail.com>
2021-07-19 10:00:29 -0500

    grid terminada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
 const menuBurguer = document.getElementById('menuBurguer');
 const listMenu = document.getElementById('listMenu');
-menuBurguer.addEventListener('click', ()=>{
+menuBurguer.addEventListener('click', () => {
     listMenu.classList.toggle('is-active')
 })
 
-// grid 
-// const data = [
-//     {"name": "Holiday", "score": 25}, 
-//     {"name": "Bargain", "score": 16}, 
-//     {"name": "Frequent", "score": 5}, 
-//     {"name": "Weekend", "score": 32},
-//   ];
-  
 const imagenes = [
     {
         "id": 1,
         "category": "branding",
-        "imgPro":  "marte.jpg"
+        "imgPro": "marte.jpg"
     },
     {
         "id": 2,
@@ -48,13 +40,9 @@ const imagenes = [
         "category": "branding",
         "imgPro": "ave.jpg"
     }
-    
-]
-//   document.querySelector('.grid').innerHTML = data.map(obj => {
-//     return `<div class="item" style="grid-column-end: span ${obj.score };"><span>${obj.name}</span></div>`;
-//   }).join('\n');
 
-  document.querySelector('.grid__container').innerHTML = imagenes.map(obj => {
-    return `<div class="grid__box"><img src="assets/img/${obj.imgPro}" alt=""></img></div>`;
-    
-  }).join('\n');
+]
+
+document.querySelector('.grid__container').innerHTML = imagenes.map(obj => {
+    return `<div class="grid__box"><img class="grid__img" src="assets/img/${obj.imgPro}" alt=""></img></div>`;
+}).join('\n');
